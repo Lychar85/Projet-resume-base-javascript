042489916657564a3a642ed30956c241b2871dd9
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2020-12-14 12:03:08 +0100

    .

---
+++ 4a- Live-writing/app.js
@@ -0,0 +1,20 @@
+
+const 
+    twit = document.getElementById("inText"),
+    result = document.getElementById("result");
+
+
+
+display()
+
+function display(){
+
+    twit.addEventListener('keyup', visible);
+
+
+}
+
+
+function visible(e){
+    result.textContent 
+}
