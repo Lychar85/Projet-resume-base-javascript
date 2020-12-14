3dc2ac0ca875124a9e17d67cff7620cee2a44b4b
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2020-12-14 14:32:25 +0100

    Update script.js

--- 5a- Inverser-chaine-caractère/script.js
+++ 5a- Inverser-chaine-caractère/script.js
@@ -1,6 +1,9 @@
 const 
     text = document.getElementById('text'),
-    textreverse = document.getElementById('textreverse');
+    textreverse = document.getElementById('textreverse'),
+    input = document.getElementById('input'),
+    button = document.getElementById('button'),
+    result = document.getElementById('result');
 
 
 display()
@@ -8,5 +11,17 @@ display()
 function display() {
     text.textContent = "wesh pelo";
     textreverse.textContent = text.textContent.split('').reverse().join('');
+    button.addEventListener('click', addtxt);
+
+
+
+
+
 }
 
+
+function addtxt(e) {
+    result.innerHTML = input.value.split('').reverse('').join('');
+
+    e.preventDefault();
+}
